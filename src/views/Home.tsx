57e16666c2b2c4afe12f86846f5abb85d35b8bf2
mazipan <mazipanneh@gmail.com>
2024-11-19 22:33:29 +0700

    FIX: Dark mode button color

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -108,7 +108,11 @@ export const Home = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <ShimmerButton>Embeed now</ShimmerButton>
+            <ShimmerButton className="shadow-2xl">
+              <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+                Embeed now
+              </span>
+            </ShimmerButton>
           </a>
         </div>
       </div>
