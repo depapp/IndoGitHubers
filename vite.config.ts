7eb2a3f34bd8b8617715f8cffd351227fd083a3e
Cristopher <32661241+Namchee@users.noreply.github.com>
2024-12-01 13:38:20 +0700

    feat: cache GitHub avatars (#70)
    * feat: cache with workbox
    
    * feat: cache avatars using workbox
    
    * chore: remove old-unused workbox deps
    
    * FIX: update maxAge to 7days
    
    ---------
    
    Co-authored-by: Irfan Maulana <mazipanneh@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
-import path from 'path';
+import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [react(), 
+    VitePWA({
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/avatars\.githubusercontent\.com\/.*/i,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'gh-avatar-cache',
+              expiration: {
+                purgeOnQuotaError: true,
+                maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              }
+            }
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
