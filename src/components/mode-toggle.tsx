1f246f2cfaed75c1fd43b6832e3fa25cf685266f
mazipan <mazipanneh@gmail.com>
2024-12-10 09:10:40 +0700

    CI: New CI to test againts pull request

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,5 +1,6 @@
 import { Moon, Sun } from 'lucide-react'
 
+import { useTheme } from '@/components/theme-provider'
 import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
@@ -7,7 +8,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { useTheme } from '@/components/theme-provider'
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
